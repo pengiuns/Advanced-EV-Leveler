29fb389c3a0e04e7713af3e89ff39bcffe658ebc
silv3rcode <gary2id@yahoo.com>
2016-09-14 00:45:40 +0300

    Trainlist fixes

--- libs/core_functions.lua
+++ libs/core_functions.lua
@@ -32,50 +32,86 @@ function isFullyTrained(name)
 	end
 end
 
-
 -- start declare
 trainlist = {}
-for i=1, #Train_Hp do
-	name = Train_Hp[i]
-	trainlist[name] = {}
+if #Train_Hp != 0 then
+	for i=1, #Train_Hp do
+		name = Train_Hp[i]
+		trainlist[name] = {}
+	end
 end
+if #Train_Spd != 0 then
+	for i=1, #Train_Spd do
+		name = Train_Spd[i]
+		trainlist[name] = {}
+	end
+end
+
+if #Train_Atk != 0 then
+	for i=1, #Train_Atk do
+		name = Train_Atk[i]
+		trainlist[name] = {}
+	end
+end
+
+if #Train_Def != 0 then
+	for i=1, #Train_Def do
+		name = Train_Def[i]
+		trainlist[name] = {}
+	end
+end
+
+if #Train_SpAtk != 0 then
+	for i=1, #Train_SpAtk do
+		name = Train_SpAtk[i]
+		trainlist[name] = {}
+	end
+end
+
+if #Train_SpDef != 0 then
+	for i=1, #Train_SpDef do
+		name = Train_SpDef[i]
+		trainlist[name] = {}
+	end
+end
+
 -- end declare
 
 -- converting config to training table's keys and values
 function convertTables()
 
 	for i=1, #Train_Hp do
-		table.insert (trainlist[Train_Hp[i]], #trainlist+1, "HP")
+		table.insert (trainlist[Train_Hp[i]], "HP")
 		
 	end
 	
 	for i=1, #Train_Atk do
 	
-		table.insert (trainlist[Train_Hp[i]], #trainlist+1, "Attack")
+		table.insert (trainlist[Train_Hp[i]], "Attack")
 		
 	end
 	
 	for i=1, #Train_Def do
 	
-		table.insert (trainlist[Train_Hp[i]], #trainlist+1, "Defense")
+		table.insert (trainlist[Train_Hp[i]], "Defense")
 		
 	end
 	
 	for i=1, #Train_Spd do
 	
-		table.insert (trainlist[Train_Hp[i]], #trainlist+1, "Speed")
+		table.insert (trainlist[Train_Hp[i]], "Speed")
 		
 	end
 
 	for i=1, #Train_SpAtk do
 	
-		table.insert (trainlist[Train_Hp[i]], #trainlist+1, "Spattack")
+		table.insert (trainlist[Train_Hp[i]], "Spattack")
 		
 	end
 
 	for i=1, #Train_SpDef do
 	
-		table.insert (trainlist[Train_Hp[i]], #trainlist+1, "Spdefense")
+		table.insert (trainlist[Train_Hp[i]], "Spdefense")
 		
 	end
 	
