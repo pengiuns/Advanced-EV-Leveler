6fd20e385bfa9314cde040f0e020a8fb66f18409
Marcel H <Migno@users.noreply.github.com>
2016-09-14 05:38:03 +0200

    Added ToDo

--- libs/KantoEvMap.lua
+++ libs/KantoEvMap.lua
@@ -1,3 +1,5 @@
+-- ToDo: Add nearest Pokecenters
+
 Hp = {"Viridian Forest", {"Caterpie", "Hoothoot"}}
 Atk = {"Route 12", {"Bellsprout", "Weepinbell"}}
 Def = {"Viridian Forest", {"Metapod", "Kakuna"}}
