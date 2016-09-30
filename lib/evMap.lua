cb5fdc3378c82ff5ca6fa7557eaa363ccc1945fc
Migno <hofferbert.marcel32@gmail.com>
2016-09-30 21:27:53 +0200

    Changed EvMap to support only Kanto currently

--- lib/evMap.lua
+++ lib/evMap.lua
@@ -9,19 +9,19 @@ if Region == "Kanto" then
 end
 
 if Region == "Johto" then
-	Hp = {"Ruins of Alph Entrance", {"Wooper", "Quagsire"}, {"Surf"}}
-	Atk = {"Route 12", {"Bellsprout", "Weepinbell"}}
-	Def = {"Viridian Forest", {"Metapod", "Kakuna"}}
-	Spd = {"Digletts Cave Entrance 2", {"Zubat", "Diglett", "Dugtrio"}}
-	SpAtk = {"Route 6", {"Psyduck", "Abra", "Magnemite"}}
-	SpDef = {"Route 19", {"Tentacool", "Tentacruel"}}
+	Hp = {"Viridian Forest", {"Caterpie", "Hoothoot"}, {"Grass"}}
+	Atk = {"Route 12", {"Bellsprout", "Weepinbell"}, {"Grass"}}
+	Def = {"Mt. Moon 1F", {"Geodude", "Onix", "Sandshrew", "Sandslash"}, {28, 51, 40, 54}}
+	Spd = {"Digletts Cave Entrance 2", {"Zubat", "Diglett", "Dugtrio"}, {17, 23, 25, 27}}
+	SpAtk = {"Route 6", {"Psyduck", "Abra", "Magnemite"}, {"Grass"}}
+	SpDef = {"Route 19", {"Tentacool", "Tentacruel"}, {"Water"}}
 end
 
 if Region == "Hoenn" then
-	Hp = {"Viridian Forest", {"Caterpie", "Hoothoot"}}
-	Atk = {"Route 12", {"Bellsprout", "Weepinbell"}}
-	Def = {"Viridian Forest", {"Metapod", "Kakuna"}}
-	Spd = {"Digletts Cave Entrance 2", {"Zubat", "Diglett", "Dugtrio"}}
-	SpAtk = {"Route 6", {"Psyduck", "Abra", "Magnemite"}}
-	SpDef = {"Route 19", {"Tentacool", "Tentacruel"}}
+	Hp = {"Viridian Forest", {"Caterpie", "Hoothoot"}, {"Grass"}}
+	Atk = {"Route 12", {"Bellsprout", "Weepinbell"}, {"Grass"}}
+	Def = {"Mt. Moon 1F", {"Geodude", "Onix", "Sandshrew", "Sandslash"}, {28, 51, 40, 54}}
+	Spd = {"Digletts Cave Entrance 2", {"Zubat", "Diglett", "Dugtrio"}, {17, 23, 25, 27}}
+	SpAtk = {"Route 6", {"Psyduck", "Abra", "Magnemite"}, {"Grass"}}
+	SpDef = {"Route 19", {"Tentacool", "Tentacruel"}, {"Water"}}
 end
